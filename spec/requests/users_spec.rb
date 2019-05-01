0a211fe4891ca007036c130655d8e68782fdfe7d
Michael Centrelli <centm860@newschool.edu>
2019-05-01 14:48:50 -0400

    Add Failure Test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -15,5 +15,17 @@ RSpec.describe 'Users Api', tpye: :request do
       expect(response.status).to eq(200)
       expect(JSON.parse(response.body)).to eq({"message"=>"Account Created"})
     end
+
+    it 'fails to create a user' do
+      params = {
+        username: 'mikec',
+        password: 'tester123',
+      }
+
+      post '/signup', params: params
+
+      expect(response.status).to eq(422)
+      expect(JSON.parse(response.body)).to eq({"message"=>"Account Not Created"})
+    end
   end
 end
