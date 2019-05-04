a064cc109f71c26921690827e0a35a564cb9ce5e
Michael Centrelli <centm860@newschool.edu>
2019-05-04 18:10:55 -0400

    Index and Destroy for Terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -2,15 +2,26 @@ class TermsController < ApplicationController
   before_action :authenticate_request!, only: [:create, :index, :destroy]
 
   def create
-    user = @current_user
-    if user.terms.create(category: term_params["category"], keyword: term_params["keyword"])
+    term = @current_user.terms.create(term_params)
+    if term.save
       render json: {message: "Term Created"}
     else
       render json: {message: "Term Not Created"}, status: 422
     end
   end
 
-  def show
+  def index
+    user = @current_user
+    render json: user.terms
+  end
+
+  def destroy
+    term = Term.find(params[:id])
+    if term.destroy
+      render json: {message: "Term Deleted"}
+    else
+      render json: {message: "Term Not Deleted"}, status: 422
+    end
   end
 
   private
